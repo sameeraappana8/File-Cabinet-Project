fa907534f66071dcde26bc852dc4d733d47a0f63
Sameera Appana <appanasa@admins-mbp.wifi.uc.edu>
2020-02-24 16:02:16 -0500

    association between user and documents

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_204929) do
+ActiveRecord::Schema.define(version: 2020_02_24_205558) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
